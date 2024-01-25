3b59955701da1533c8c65ac9221ce639a63020fc
ykob <info@tplh.net>
2024-01-25 22:53:31 +0900

    added r3f sample box.

--- app/routes/_main._index/route.tsx
+++ app/routes/_main._index/route.tsx
@@ -1,13 +1,49 @@
 import { json } from "@remix-run/node";
+import { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 
 export const loader = () => {
   return json({});
 };
 
+function Box(props: JSX.IntrinsicElements["mesh"]) {
+  const meshRef = useRef<THREE.Mesh>(null!);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  useFrame((_state, delta) => {
+    return (meshRef.current!.rotation.x += delta);
+  });
+
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
+
 export default function Home() {
   return (
-    <div>
-      <p>Hello world!</p>
-    </div>
+    <Canvas>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
   );
 }
--- app/routes/_main/route.tsx
+++ app/routes/_main/route.tsx
@@ -3,7 +3,6 @@ import { Outlet } from "@remix-run/react";
 export default function MainLayout() {
   return (
     <div>
-      <h1>Hello world!</h1>
       <Outlet />
     </div>
   );
